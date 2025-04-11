dfee5add6af5360eab46a76bed0284945e17d2b7
D33key <dimakisa777@yandex.ru>
2025-04-11 11:39:33 +0300

    fix: add prod url to query

--- shared/hooks/use-query.ts
+++ shared/hooks/use-query.ts
@@ -9,7 +9,6 @@ export default function useQuery<T extends QuestionResponse>(
 	queryOptions: KeysQueryOptionsObject,
 	isPagination = false,
 ) {
-	console.log('queryOptions URL in useQuery', queryOptions)
 	const { data, ...other } = useSuspenseQuery<
 		T,
 		Error,
