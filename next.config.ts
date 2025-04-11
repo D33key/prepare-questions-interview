7103ede3315bcacd1f331089e2b135488d5db235
D33key <dimakisa777@yandex.ru>
2025-04-11 11:01:48 +0300

    config: add config to enable github pages

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-	// output: 'export',
-  // distDir: 'dist',
+	output: 'export',
+	basePath: '/prepare-questions-interview',
+	images: {
+		unoptimized: true,
+	},
 };
 
 export default nextConfig;
