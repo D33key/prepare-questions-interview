fa3935b6b7fbbd8c92b002e7d9457ec5fae1d0d7
D33key <dimakisa777@yandex.ru>
2025-04-11 12:15:43 +0300

    fix: remove adding pagination boolean in query

--- app/api/_utils/generateQueryOptions.ts
+++ app/api/_utils/generateQueryOptions.ts
@@ -5,13 +5,17 @@ export default function generateQueryOptions(url: URLS_KEYS_TYPE) {
 	return queryOptions({
 		queryKey: [url],
 		queryFn: async () => {
-			const basePath =
-				process.env.NEXT_PUBLIC_BASE_URL ??
-				'd33key.github.io/prepare-questions-interview';
+			try {
+				const basePath =
+					process.env.NEXT_PUBLIC_BASE_URL ??
+					'd33key.github.io/prepare-questions-interview';
 
-			const response = await fetch(`${basePath}/api/${url}.json`);
+				const response = await fetch(`${basePath}/api/${url}.json`);
 
-			return response.json();
+				return response.json();
+			} catch (error) {
+				console.log(error);
+			}
 		},
 	});
 }
--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,15 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
 	output: 'export',
 	basePath:
-		process.env.NODE_ENV === 'production'
-			? '/prepare-questions-interview'
-			: '',
+		process.env.NODE_ENV === 'production' ? '/prepare-questions-interview' : '',
 	images: {
 		unoptimized: true,
+	},
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+	publicRuntimeConfig: {
+		NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
 	},
 };
 
