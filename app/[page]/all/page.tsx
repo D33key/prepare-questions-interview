156e50040779cc0e90e4a09560b0c64ba3484125
D33key <dimakisa777@yandex.ru>
2025-04-12 13:51:59 +0300

    feat: add seo optimization

--- app/[page]/all/page.tsx
+++ app/[page]/all/page.tsx
@@ -1,6 +1,7 @@
 import { PageParams } from '@/app/[page]/_type';
 import AccordionCards from '@/entities/accordion';
 import { URLS_KEYS, type URLS_KEYS_TYPE } from '@/shared/constants/url';
+import capitalizeFirstLetter from '@/shared/utils/capitalizeFirstLetter';
 import { redirect } from 'next/navigation';
 
 interface StaticParams {
@@ -9,6 +10,17 @@ interface StaticParams {
 
 export function generateStaticParams() {
 	return [{ page: 'javascript' }, { page: 'react' }] as StaticParams[];
+}
+
+export async function generateMetadata({ params }: PageParams) {
+	const { page } = await params;
+
+	const capitalizedTitle = capitalizeFirstLetter(page);
+
+	return {
+		title: `Вопросы по ${capitalizedTitle}`,
+		description: `Все ключевые вопросы по ${capitalizedTitle} для собеседования: от основ до сложных тем. Четкие ответы с примерами кода. Проверь себя и подготовься к интервью!`,
+	};
 }
 
 export default async function Page({ params }: PageParams) {
--- app/[page]/random/page.tsx
+++ app/[page]/random/page.tsx
@@ -1,6 +1,7 @@
 import { PageParams } from '@/app/[page]/_type';
 import RandomCards from '@/features/card/ui';
 import { URLS_KEYS, type URLS_KEYS_TYPE } from '@/shared/constants/url';
+import capitalizeFirstLetter from '@/shared/utils/capitalizeFirstLetter';
 import { redirect } from 'next/navigation';
 
 interface StaticParams {
@@ -9,6 +10,17 @@ interface StaticParams {
 
 export function generateStaticParams() {
 	return [{ page: 'javascript' }, { page: 'react' }] as StaticParams[];
+}
+
+export async function generateMetadata({ params }: PageParams) {
+	const { page } = await params;
+
+	const capitalizedTitle = capitalizeFirstLetter(page);
+
+	return {
+		title: `Вопросы по ${capitalizedTitle} | Карточки`,
+		description: `Случайные карточки по вопросам на тему ${capitalizedTitle} для собеседования. Проверь себя и подготовься к интервью!`,
+	};
 }
 
 export default async function Page({ params }: PageParams) {
